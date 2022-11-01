3669dcea987628e2ae73c9215669b839d5d0f493
Fernando Campos Silva Dal' Maria <fernando.csm123@gmail.com>
2022-11-01 12:05:37 -0300

    Pilha Flexivel em C

---
+++ EstruturasFlexiveis/OutrasEstruturas/Stack/C/header/Stack.h
@@ -0,0 +1,30 @@
+
+#ifndef STACK
+#define STACK
+
+//=====INCLUDES=====//
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "Node.h"
+
+//=====STRUCT=====//
+typedef struct stack {
+    Node* first;
+    Node* last;
+    int len;
+} Stack;
+
+//=====PROTOTYPES=====//
+Stack* construct();
+void clear(Stack* const stack);
+void destruct(Stack* const stack);
+
+bool isEmptys(const Stack* const stack);
+void push(Stack* const stack, int obj);
+int pop(Stack* const stack);
+int peek(const Stack* const stack);
+
+void prints(const Stack* const stack);
+
+#endif
