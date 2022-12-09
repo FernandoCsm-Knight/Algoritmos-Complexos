d48461c9da9a69ce28e3d291ab7f7f329eee10a1
Fernando Campos Silva Dal' Maria <fernando.csm123@gmail.com>
2022-12-09 19:10:13 -0300

    fix: tests trie tree

--- Arvores/ArvoreTrie/TrieLinkedList/Test.java
+++ Arvores/ArvoreTrie/TrieLinkedList/Test.java
@@ -3,17 +3,11 @@ package Arvores.ArvoreTrie.TrieLinkedList;
 public class Test {
    
    public static void main(String[] args) {
-      Tree tree = new Tree("Fer", "Fernando", "Fernanda", "Max", "Do", "Val", "Mchado");
+      Tree tree = new Tree("Fer", "Fernando", "Fernanda");
 
       tree.prefix();
       System.out.println();
       tree.prefix("Fernan");
-      System.out.println();
-      tree.prefix("Ma");
-      System.out.println();
-      tree.prefix("Mal");
-      System.out.println();
-      tree.prefix("M");
    }
 
 }
